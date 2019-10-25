26414b943ee7f07e72930363a451d252b1f4fdc9
Irvin Coronado Salcedo <55162740+IrvinCS2002@users.noreply.github.com>
2019-10-25 17:07:30 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 int main (){
     int opcion=0;
@@ -13,6 +14,7 @@ int main (){
         case 1:
             cout<<"Hola";
             break;
+            
     }
     return 0;
 }
