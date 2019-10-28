2ccee23231269800a4fa3f177eb2a21e1f7a350c
IrvinCS2002 <irvin.coronado.salcedo@gmail.com>
2019-10-27 20:21:59 -0500

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,44 +1,45 @@
-#include <iostream>
-#include <stdlib.h>
-#include <string.h>
-#include <fstream>
+#include<iostream>
+#include<stdlib.h>
+#include<string.h>
+#include<fstream>
 using namespace std;
-void lectura(){
-    ifstream archivo;
-    string texto;
-    //Abrimos el archivo en modo lectura
-    archivo.open("PorqueEllaNoMeAma.txt",ios::in);
-    if (archivo.fail()){
-        cout<<"No se pudo abrir el texto";
-        exit(1);
-    }
-    while(!archivo.eof()){
-        getline(archivo,texto);
-        cout<<texto<<endl;
-    }
-    //cerramos el archivo
-    archivo.close();
-}
-int main() {
-    lectura();
+void lectura();
+int main(){
     int opcion=0;
-    cout<<"1. Leer archivo"<<endl;
+    cout<<"1. Leer archivo txt"<<endl;
     cout<<"2. Guardar archivo"<<endl;
     cout<<"3. Justificar texto"<<endl;
     cout<<"4. Alinear texto a la izquierda"<<endl;
     cout<<"5. Alinear texto a la derecha"<<endl;
-    cout<<"Eliga opcion:";
+    cout<<"Elige alguna opcion:";
     cin>>opcion;
     switch(opcion){
         case 1:
-            cout<<"Hola mundo";
+            lectura();
             break;
         case 2:
-            cout <<"";
-            break;
-        case 3:
-            cout<<"";
+            cout<<"Hola";
             break;
     }
     return 0;
+}
+
+void lectura(){
+    ifstream archivo;
+    string nombreArchivo,texto;
+    cout<<"Digite el nombre o la ubicacion del archivo o fichero: ";
+    getline(cin,nombreArchivo);
+    archivo.open(nombreArchivo.c_str(),ios::in); //Abrimos el archivo en modo lectura
+
+    if(archivo.fail()){
+        cout<<"No se pudo abrir el archivo";
+        exit(1);
+    }
+
+    while(!archivo.eof()){ //mientras no sea final del archivo
+        getline(archivo,texto);
+        cout<<texto<<endl;
+    }
+
+    archivo.close(); //Cerramos el archivo
 }
