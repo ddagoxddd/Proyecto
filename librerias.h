857d84d71f13b43794179768d640253f03e740fe
David Gabriel <davidgabo@MacBook-Air-de-David.local>
2019-11-26 21:54:22 -0500

    llamado de librerías

---
+++ librerias.h
@@ -0,0 +1,17 @@
+#ifndef PROYECTO_LIBRERIAS_H
+#define PROYECTO_LIBRERIAS_H
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <fstream>
+#include <iomanip>
+#include <stdio.h>
+#include <utility>
+
+using namespace std;
+
+#endif //PROYECTO_LIBRERIAS_H
